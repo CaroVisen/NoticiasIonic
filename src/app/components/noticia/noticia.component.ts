59feacdbec6a69b245fab9c265ddac1f8bccbeb9
CaroVisen <carolinaaylen26@gmail.com>
2020-01-25 18:15:13 -0300

    105 hecho

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -10,13 +11,41 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
 
-  constructor( private iab: InAppBrowser) { }
+  constructor( private iab: InAppBrowser, private  actionSheetController: ActionSheetController) { }
 
   ngOnInit() {}
 
-  abrirNoticia(){
-    //console.log(this.noticia.url);
+  async abrirNoticia(){
     const browser = this.iab.create(this.noticia.url, '_system');
   }
+
+  async lanzarMenu(){
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Agregar a Favoritos',
+        icon: 'heart',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: 'action-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }  
 
 }
